3c2dcc5d2ce8bbd3e0f976192867564f3793dba0
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2024-10-16 01:13:48 +0800

    trajectory calc

--- Assets/Scripts/TrajectoryCalc.cs
+++ Assets/Scripts/TrajectoryCalc.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TrajectoryCalc : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    [SerializeField] GameObject item;
+    [SerializeField] float initialVelocity;
+    [SerializeField] float angle;
+    [SerializeField] TextMeshProUGUI timeDisplay, velDisplay, angleDisplay, distanceDisplay;
+    public bool isPressed;
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isPressed = true;
+            LaunchItem(item, initialVelocity, angle);
 
+            float timeTaken = CalculateTimeofFlight(initialVelocity, angle);
+            timeDisplay.SetText(Mathf.Abs(timeTaken) + " s");
+
+            float dist = DistanceTraveled(initialVelocity, angle);
+            distanceDisplay.SetText(Mathf.Abs(dist) + " m");
+
+        }
+        angleDisplay.SetText(angle + " θ");
+        velDisplay.SetText("" + initialVelocity + " m/s");
+    }
+    public void LaunchItem(GameObject obj, float initialVelocity, float angle)
+    {
+        Rigidbody2D rb = obj.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            float angleRad = angle * Mathf.Deg2Rad;
+
+            Vector2 velocity = new Vector2(initialVelocity * Mathf.Cos(angleRad), initialVelocity * Mathf.Sin(angleRad));
+
+            rb.velocity = velocity;
+            rb.constraints = RigidbodyConstraints2D.None;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the object has collided with the ground
+        if (collision.gameObject.CompareTag("Ground")) // Ensure your ground has the "Ground" tag
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                // Freeze the Rigidbody2D's position to stop sliding
+                rb.constraints = RigidbodyConstraints2D.FreezeAll; // Freeze all movement
+            }
+        }
+    }
+    public float CalculateTimeofFlight(float initialVelocity, float angle)
+    {
+        float angleRad = angle * Mathf.Deg2Rad;
+
+        float timeOfFlight = 2 * initialVelocity * Mathf.Sin(angleRad) / Physics.gravity.y;
+        return timeOfFlight;
+    }
+    public float DistanceTraveled(float initialVelocity, float angle)
+    {
+        float angleRad = angle * Mathf.Deg2Rad;
+        float distance = (initialVelocity * initialVelocity * Mathf.Sin(angleRad)) / Physics.gravity.y;
+
+        return distance;
     }
 }
